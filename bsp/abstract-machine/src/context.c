cbc5589f407793b20f1d2dcf3ed1fce2370b4a44
Letong Han <hanlt@shanghaitech.edu.cn>
2024-06-28 15:27:24 +0800

    [feat] PA4.1 rt-thread part

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -2,9 +2,21 @@
 #include <klib.h>
 #include <rtthread.h>
 
-static Context* ev_handler(Event e, Context *c) {
+static Context *ev_handler(Event e, Context *c) {
+  Context ***from_to = (Context ***)rt_thread_self()->user_data;
+  Context **from, **to;
   switch (e.event) {
-    default: printf("Unhandled event ID = %d\n", e.event); assert(0);
+  case EVENT_YIELD:
+    from = from_to[0];
+    to = from_to[1];
+    if (from) *from = c;
+    c = *to;
+    break;
+  case EVENT_IRQ_TIMER:
+    break;
+  default:
+    printf("Unhandled event ID = %d\n", e.event);
+    assert(0);
   }
   return c;
 }
@@ -13,19 +25,37 @@ void __am_cte_init() {
   cte_init(ev_handler);
 }
 
-void rt_hw_context_switch_to(rt_ubase_t to) {
-  assert(0);
+void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
+  rt_ubase_t prev_user_data = rt_thread_self()->user_data;
+  rt_ubase_t from_to[2] = {from, to};
+  rt_thread_self()->user_data = (rt_ubase_t)from_to;
+  yield();
+  rt_thread_self()->user_data = prev_user_data;
 }
 
-void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
-  assert(0);
+void rt_hw_context_switch_to(rt_ubase_t to) {
+  rt_hw_context_switch(0, to);
 }
 
 void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t to, struct rt_thread *to_thread) {
   assert(0);
 }
 
+static void wrapper_entry(void *arg) {
+  void **args = arg;
+  void (*tentry)(void *) = args[0];
+  void *parameter = args[1];
+  void (*texit)(void) = args[2];
+  tentry(parameter);
+  texit();
+}
+
 rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit) {
-  assert(0);
-  return NULL;
+  stack_addr = (rt_uint8_t*)((uintptr_t)stack_addr & ~(sizeof(uintptr_t) - 1));  
+  stack_addr -= 3 * sizeof(uintptr_t);
+  void **args = (void **)stack_addr;
+  args[0] = tentry;
+  args[1] = parameter;
+  args[2] = texit;
+  return (rt_uint8_t *)kcontext((Area){stack_addr - sizeof(Context) - sizeof(uintptr_t), stack_addr}, wrapper_entry, args);  
 }
